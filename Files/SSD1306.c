752b1e43403e8c358554283c4674b79ed796907e
Tekl7 <teclvojtech@gmail.com>
2019-07-23 19:44:49 +0200

    GLCD_DrawBitmap fix
    Update last line condition fixed.

--- Files/SSD1306.c
+++ Files/SSD1306.c
@@ -371,7 +371,8 @@ void GLCD_DrawBitmap(const uint8_t *Bitmap, uint8_t Width, const uint8_t Height,
 	}
 
 	//#6 - Update last line, if needed
-	if (lines > 1)
+	//If (LINE_STARTING != LINE_ENDING)
+	if (__GLCD_getLine(y2) != __GLCD_getLine((y2 + __GLCD.Font.Height - 1)) && y < __GLCD_Screen_Height)
 	{
 		//Go to the start of the line
 		GLCD_GotoXY(x, y);
