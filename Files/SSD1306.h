8a985f62ac0af59d414be05365ea9132a4ae4889
Efthymios Koktsidis <efthymios-ks@users.noreply.github.com>
2020-07-09 17:50:29 +0300

    Update SSD1306.h

--- Files/SSD1306.h
+++ Files/SSD1306.h
@@ -3,12 +3,12 @@
 /*
 ||
 ||  Filename:	 		SSD1306.h
-||  Title: 			    SSD1306 Driver
+||  Title: 			SSD1306 Driver
 ||  Author: 			Efthymios Koktsidis
-||	Email:				efthymios.ks@gmail.com
+||  Email:			efthymios.ks@gmail.com
 ||  Compiler:		 	AVR-GCC
-||	Description:
-||	This library can drive SSD1306 based GLCD.
+||  Description:
+||  This library can drive SSD1306 based GLCD.
 ||
 */
 
@@ -30,89 +30,82 @@ extern "C"
 #endif
 
 //----- Auxiliary data ---------------------------//
-#define __GLCD_I2C_Address							0x3C	//0x3C or 0x3D
+#define __GLCD_I2C_Address					0x3C	//0x3C or 0x3D
 
-#define __GLCD_RW									0
-#define __GLCD_SA0									1
-#define __GLCD_DC									6
-#define __GLCD_CO									7
+#define __GLCD_RW						0
+#define __GLCD_SA0						1
+#define __GLCD_DC						6
+#define __GLCD_CO						7
 
-#if (GLCD_Size == GLCD_128_64)
+#if defined(GLCD_128_64)
 	#define __GLCD_Screen_Width          			128
 	#define __GLCD_Screen_Height         			64
-#elif (GLCD_Size == GLCD_128x32)
+#elif defined(GLCD_128_32)
 	#define __GLCD_Screen_Width         			128
 	#define __GLCD_Screen_Height        			32
-#else
+#elif defined(GLCD_96_16)
 	#define __GLCD_Screen_Width          			96
 	#define __GLCD_Screen_Height         			16
 #endif
-#define	__GLCD_Screen_Line_Height					8
-#define __GLCD_Screen_Lines							__GLCD_Screen_Height / __GLCD_Screen_Line_Height
+#define	__GLCD_Screen_Line_Height				8
+#define __GLCD_Screen_Lines					(__GLCD_Screen_Height / __GLCD_Screen_Line_Height)
 
 //Fundamental Command Table
-#define __GLCD_Command_Display_On							0xAF
-#define __GLCD_Command_Display_Off							0xAE
-#define __GLCD_Command_Constrast_Set						0x81
-#define __GLCD_Command_Display_All_On_Resume				0xA4
-#define __GLCD_Command_Display_All_On						0xA5
-#define __GLCD_Command_Display_Normal						0xA6
-#define __GLCD_Command_Display_Inverse						0xA7
+#define __GLCD_Command_Display_On				0xAF
+#define __GLCD_Command_Display_Off				0xAE
+#define __GLCD_Command_Constrast_Set				0x81
+#define __GLCD_Command_Display_All_On_Resume			0xA4
+#define __GLCD_Command_Display_All_On				0xA5
+#define __GLCD_Command_Display_Normal				0xA6
+#define __GLCD_Command_Display_Inverse				0xA7
 
-//Scrolling Command Table
-#define __GLCD_Command_Scroll_Activate						0x2F
-#define __GLCD_Command_Scroll_Deactivate					0x2E	
-#define __GLCD_Command_Scroll_Left							0x27
-#define __GLCD_Command_Scroll_Right							0x26
-#define __GLCD_Command_Scroll_Vertical_Left					0x2A
-#define __GLCD_Commad_Scroll_Vertical_Right					0x29
-#define __GLCD_Command_Scroll_Vertical_Area_Set				0xA3
+//Scrolling Command Tab
+#define __GLCD_Command_Scroll_Activate				0x2F
+#define __GLCD_Command_Scroll_Deactivate			0x2E	
+#define __GLCD_Command_Scroll_Left				0x27
+#define __GLCD_Command_Scroll_Right				0x26
+#define __GLCD_Command_Scroll_Vertical_Left			0x2A
+#define __GLCD_Commad_Scroll_Vertical_Right			0x29
+#define __GLCD_Command_Scroll_Vertical_Area_Set			0xA3
 
 //Addressing Setting Command Table
 #define __GLCD_Command_Page_Addressing_Column_Lower_Set		0x00
 #define __GLCD_Command_Page_Addressing_Column_Higher_Set	0x10
 #define __GLCD_Command_Page_Addressing_Page_Start_Set		0xB0
-#define __GLCD_Command_Page_Address_Set						0x22
-#define __GLCD_Command_Memory_Addressing_Set				0x20
-#define __GLCD_Command_Column_Address_Set					0x21
+#define __GLCD_Command_Page_Address_Set				0x22
+#define __GLCD_Command_Memory_Addressing_Set			0x20
+#define __GLCD_Command_Column_Address_Set			0x21
 
 //Hardware Configuration
-#define __GLCD_Command_Display_Start_Line_Set				0x40
-#define __GLCD_Command_Display_Offset_Set					0xD3
-#define __GLCD_Command_Segment_Remap_Set					0xA0
-#define __GLCD_Command_Multiplex_Radio_Set					0xA8
-#define __GLCD_Command_Com_Output_Scan_Inc					0xC0
-#define __GLCD_Command_Com_Output_Scan_Dec					0xC8
-#define __GLCD_Command_Com_Pins_Set							0xDA
+#define __GLCD_Command_Display_Start_Line_Set			0x40
+#define __GLCD_Command_Display_Offset_Set			0xD3
+#define __GLCD_Command_Segment_Remap_Set			0xA0
+#define __GLCD_Command_Multiplex_Radio_Set			0xA8
+#define __GLCD_Command_Com_Output_Scan_Inc			0xC0
+#define __GLCD_Command_Com_Output_Scan_Dec			0xC8
+#define __GLCD_Command_Com_Pins_Set				0xDA
 
 //Timing and Driving Scheme Setting Command Table
-#define __GLCD_Command_Display_Clock_Div_Ratio_Set			0xD5
+#define __GLCD_Command_Display_Clock_Div_Ratio_Set		0xD5
 #define __GLCD_Command_Display_Oscillator_Frequency_Set		0xD5
-#define __GLCD_Command_Precharge_Period_Set					0xD9
-#define __GLCD_Command_VCOMH_Deselect_Level_Set				0xDB
-#define __GLCD_Command_Nop									0xE3
+#define __GLCD_Command_Precharge_Period_Set			0xD9
+#define __GLCD_Command_VCOMH_Deselect_Level_Set			0xDB
+#define __GLCD_Command_Nop					0xE3
 
 //Charge Pump Command Table
-#define __GLCD_Command_Charge_Pump_Set						0x8D
+#define __GLCD_Command_Charge_Pump_Set				0x8D
 
 //Reset delays (in ms)
-#define _GLCD_Delay_1										1
-#define _GLCD_Delay_2										10
+#define _GLCD_Delay_1						1
+#define _GLCD_Delay_2						10
 
 #if (GLCD_Error_Checking != 0)
-	enum GLCD_Status
+	enum GLCD_Status_t
 	{
 		GLCD_Ok,
 		GLCD_Error
 	};
 #endif
-
-enum GLCD_Type_t
-{
-	GLCD_128_64,
-	GLCD_128x32,
-	GLCD_96x16
-};
 
 enum OperatingMode_t
 {
@@ -144,7 +137,7 @@ typedef struct
 typedef struct
 {
 	#if (GLCD_Error_Checking != 0)
-		enum GLCD_Status Status;
+		enum GLCD_Status_t Status;
 	#endif
 	uint8_t X;
 	uint8_t Y;
@@ -159,7 +152,7 @@ void GLCD_SendData(const uint8_t Data);
 void GLCD_Setup(void);
 void GLCD_Reset(void);
 #if (GLCD_Error_Checking != 0)
-	enum GLCD_Status GLCD_Status(void);
+	enum GLCD_Status_t GLCD_GetStatus(void);
 #endif
 void GLCD_Render(void);
 void GLCD_SetDisplay(const uint8_t On);
